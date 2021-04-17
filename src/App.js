60d9be55f9850d4ccb56437666f5873e5e1939ad
MikeBao99 <mikebao@college.harvard.edu>
2021-04-17 16:03:57 -0400

    Merge conflicts

--- src/App.js
+++ src/App.js
@@ -1,17 +1,13 @@
-import React, { Component } from "react";
+import React from "react";
 import logo from './logo.svg';
 import './App.css';
 import Maps from './components/Maps';
 import Form from './components/Form';
 import Search from './components/Search'
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
-<<<<<<< HEAD
-import {Fragment} from 'react';
-=======
 import { Component, Fragment } from 'react';
 import OurNav from './components/OurNav';
 import 'bootstrap/dist/css/bootstrap.min.css';
->>>>>>> 74035459cd2725673ca9fd7e22f6b19cca01c25a
 
 class App extends Component{
   constructor(props) {
@@ -31,34 +27,12 @@ class App extends Component{
     }
   render () {
   return (
-<<<<<<< HEAD
-    <>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-        <Search parentCallback = {this.handleCallback}/>
-        <Maps position = {this.state.position}/>
-      </>
-  );}
-=======
     <div>
       <OurNav />
-      <Search />
-      <Maps />
+      <Search parentCallback = {this.handleCallback}/>
+        <Maps position = {this.state.position}/>
     </div>
   );
->>>>>>> 74035459cd2725673ca9fd7e22f6b19cca01c25a
-}
+}}
 
 export default App;
