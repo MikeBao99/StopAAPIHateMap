66168f62e191e981d9d9caf50d9cfd6eff3662ef
carolynwanf <54962094+carolynwanf@users.noreply.github.com>
2021-04-17 14:35:17 -0700

    less jank

--- src/components/Home.js
+++ src/components/Home.js
@@ -18,27 +18,31 @@ const Home = () => {
         < div >
             <Container>
                 <Row className="justify-content-md-center" style={rowStyle}>
-                    <Col>
-                        <h2>welcome to</h2>
-                        <h1 style={{ fontSize: '10vmin', color: 'crimson' }}>EMPOWER</h1>
-                    </Col>
                     <Col >
                         <img style={{ height: '50vmin', margin: '0 0 5vmin 0' }} className='srcimg' src={`${MY_DOMAIN}/empower.png`}></img>
                     </Col>
+                    <Col style={{ marginTop: '5vmin' }}>
+                        <h2>welcome to</h2>
+                        <h1 style={{ fontSize: '10vmin', color: 'crimson' }}>EMPOWER</h1>
+                        <div style={{ marginTop: '5vmin', marginLeft: '2vmin' }}>
+                            <Row>
+                                <Button style={{ marginRight: '1vmin' }} href="/report" variant='danger'>Report</Button>
+                                <h3> an incident</h3>
+                            </Row>
+                            <Row style={{ marginTop: '2vmin' }}>
+                                <Button style={{ marginRight: '1vmin' }} href="/request" variant='danger'>Request</Button>
+                                <h3> help</h3>
+                            </Row>
+                            <Row style={{ marginTop: '2vmin' }}>
+                                <Button style={{ marginRight: '1vmin' }} href="/signup" variant='outline-danger'>Sign Up</Button>
+                                <h3>for updates</h3>
+                            </Row>
+                        </div>
+                    </Col>
+
                 </Row>
-                <div style={{ marginTop: '44vmin', marginLeft: '20vmin' }}>
-                    <Row>
-                        <Button style={{ marginRight: '1vmin' }} href="/report" variant='danger'>Report</Button>
-                        <h3> an incident</h3>
-                    </Row>
-                    <Row style={{ marginTop: '2vmin' }}>
-                        <Button style={{ marginRight: '1vmin' }} href="/request" variant='danger'>Request</Button>
-                        <h3> help</h3>
-                    </Row>
-                    <Row style={{ marginTop: '2vmin' }}>
-                        <Button style={{ marginRight: '1vmin' }} href="/signup" variant='outline-danger'>Sign Up</Button>
-                        <h3>for updates</h3>
-                    </Row>
+                <div style={{ marginTop: '30vmin', marginLeft: '75vmin' }}>
+
                 </div>
 
 
